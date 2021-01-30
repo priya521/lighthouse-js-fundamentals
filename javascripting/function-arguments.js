f7ce46db8930c49a9aa3ca15ee4fd42d38473b7a
priya521 <priya521.j@gmail.com>
2021-01-30 01:13:42 +0000

    Use conditionals to tell people about the weather

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,10 @@
+function math(firstArg,secondArg,thirdArg){
+let a=secondArg*thirdArg;
+let b=firstArg+a;
+return b
+
+}
+
+
+console.log(math(53,61,67))
+
---
+++ javascripting/variables.js
@@ -0,0 +1,4 @@
+let example = "some string"
+console.log(example);
+//let love ="satya"
+//console.log(love);
